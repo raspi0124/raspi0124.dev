61a50f46786e0666778f0add95cd75211b7ead00
raspi0124 <raspi0124@gmail.com>
2023-12-16 19:46:09 +0900

    Centerタグが消えた

--- pages/links.tsx
+++ pages/links.tsx
@@ -6,66 +6,64 @@ const TopDisplay: NextPage = () => {
   return (
     <>
       <div className="linksContainer">
-        <center>
+        <Typography
+          sx={{ typography: { md: "h3", xs: "h3" } }}
+          style={{ padding: "2rem" }}
+          color="common.white"
+        >
+          相互リンク一覧 (ABC順)
+        </Typography>
+        <Typography style={{ padding: "2rem" }} color="common.white">
+          (見た目が最悪なのでそのうち見た目をよくします)
+        </Typography>
+        <div style={{ padding: "2rem" }}>
+          <Typography variant="h4" color="common.white">
+            <a
+              href="https://eka.earth/"
+              rel="friend met co-worker colleague neighbor"
+              style={{ textDecoration: "underline" }}
+            >
+              Ekasilicon
+            </a>
+          </Typography>
+        </div>
+        <div style={{ padding: "2rem" }}>
           <Typography
-            sx={{ typography: { md: "h3", xs: "h3" } }}
-            style={{ padding: "2rem" }}
+            variant="h4"
             color="common.white"
+            style={{ textDecoration: "underline" }}
           >
-            相互リンク一覧 (ABC順)
+            <a
+              href="https://www.eniehack.net/~eniehack/"
+              rel="friend met co-worker colleague neighbor"
+            >
+              Eniehack
+            </a>
           </Typography>
-          <Typography style={{ padding: "2rem" }} color="common.white">
-            (見た目が最悪なのでそのうち見た目をよくします)
+        </div>
+        <div style={{ padding: "2rem" }}>
+          <Typography
+            variant="h4"
+            color="common.white"
+            style={{ textDecoration: "underline" }}
+          >
+            <a
+              href="https://lai-lai.info/"
+              rel="friend met co-worker colleague neighbor"
+            >
+              らいらい
+            </a>
           </Typography>
-          <div style={{ padding: "2rem" }}>
-            <Typography variant="h4" color="common.white">
-              <a
-                href="https://eka.earth/"
-                rel="friend met co-worker colleague neighbor"
-                style={{ textDecoration: "underline" }}
-              >
-                Ekasilicon
-              </a>
-            </Typography>
-          </div>
-          <div style={{ padding: "2rem" }}>
-            <Typography
-              variant="h4"
-              color="common.white"
-              style={{ textDecoration: "underline" }}
-            >
-              <a
-                href="https://www.eniehack.net/~eniehack/"
-                rel="friend met co-worker colleague neighbor"
-              >
-                Eniehack
-              </a>
-            </Typography>
-          </div>
-          <div style={{ padding: "2rem" }}>
-            <Typography
-              variant="h4"
-              color="common.white"
-              style={{ textDecoration: "underline" }}
-            >
-              <a
-                href="https://lai-lai.info/"
-                rel="friend met co-worker colleague neighbor"
-              >
-                らいらい
-              </a>
-            </Typography>
-          </div>
-          <div style={{ padding: "2rem" }}>
-            <Typography
-              variant="h5"
-              color="common.white"
-              style={{ textDecoration: "underline" }}
-            >
-              <Link href="/">←TOPに戻る</Link>
-            </Typography>
-          </div>
-        </center>
+        </div>
+        <div style={{ padding: "2rem" }}>
+          <Typography
+            variant="h5"
+            color="common.white"
+            style={{ textDecoration: "underline" }}
+          >
+            <Link href="/">←TOPに戻る</Link>
+          </Typography>
+        </div>
       </div>
     </>
   );

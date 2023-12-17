de233a390f8de37c4e341d76808ba2edd6b14b2e
raspi0124 <raspi0124@gmail.com>
2023-12-17 16:44:40 +0900

    画像をきちんとImageタグを使うようにした altもある

--- pages/components/TopDisplay.tsx
+++ pages/components/TopDisplay.tsx
@@ -3,16 +3,21 @@ import { Grid } from "@mui/material";
 import Typography from "@mui/material/Typography";
 import Box from "@mui/material/Box";
 import Link from "../../node_modules/next/link";
+import Image from "next/image";
+import raspi0124png from "../../public/raspi0124.png";
+
 const TopDisplay: NextPage = () => {
   return (
     <>
       <div className="homeContainer">
         <Grid container spacing={0}>
           <Grid item xs={12} md={3}>
-            <img
-              src="https://raspi0124.dev/raspi0124.png"
-              min-height="50vh"
-              width="100%"
+            <Image
+              src={raspi0124png}
+              alt="raspi0124のアイコン デフォルメされたかわいい熊がすまし顔で青い鞄を持っている"
+              layout="responsive"
+              width={1000}
+              height={500}
             />
           </Grid>
           <Grid className="center" item xs={12} md={9}>

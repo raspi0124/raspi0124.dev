691cfb3284e4f946ae5ff299552bab7a001ff193
raspi0124 <raspi0124@gmail.com>
2023-12-30 01:37:52 +0900

    Made img undraggavble/unselectable

--- pages/components/TopDisplay.tsx
+++ pages/components/TopDisplay.tsx
@@ -12,6 +12,7 @@ const TopDisplay = () => (
       <Grid container spacing={0}>
         <Grid item xs={12} md={3}>
           <Image
+            className="unselectable"
             loader={({ src }) => src}
             src={raspi0124png}
             alt="raspi0124のアイコン デフォルメされたかわいい熊がすまし顔で青い鞄を持っている"

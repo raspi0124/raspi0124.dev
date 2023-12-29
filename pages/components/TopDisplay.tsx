f4823ca50d86e9edc5e90fb157a7ef9440176986
raspi0124 <raspi0124@gmail.com>
2023-12-30 01:18:20 +0900

    Updated syntax of TopDisplay

--- pages/components/TopDisplay.tsx
+++ pages/components/TopDisplay.tsx
@@ -6,70 +6,68 @@ import Link from "../../node_modules/next/link";
 import Image from "next/image";
 import raspi0124png from "../../public/raspi0124.png";
 
-const TopDisplay: NextPage = () => {
-  return (
-    <>
-      <div className="homeContainer">
-        <Grid container spacing={0}>
-          <Grid item xs={12} md={3}>
-            <Image
-              loader={({ src }) => src}
-              src={raspi0124png}
-              alt="raspi0124のアイコン デフォルメされたかわいい熊がすまし顔で青い鞄を持っている"
-              layout="responsive"
-              width={1000}
-              height={1000}
-            />
-          </Grid>
-          <Grid className="center" item xs={12} md={9}>
-            <Box>
-              <Typography
-                sx={{ typography: { md: "h1", xs: "h2" } }}
-                style={{ paddingBottom: "2rem" }}
-                color="common.white"
-              >
-                raspi0124
-              </Typography>
-            </Box>
-          </Grid>
+const TopDisplay = () => (
+  <>
+    <div className="homeContainer">
+      <Grid container spacing={0}>
+        <Grid item xs={12} md={3}>
+          <Image
+            loader={({ src }) => src}
+            src={raspi0124png}
+            alt="raspi0124のアイコン デフォルメされたかわいい熊がすまし顔で青い鞄を持っている"
+            layout="responsive"
+            width={1000}
+            height={1000}
+          />
         </Grid>
-        <Grid className="menu" container spacing={3}>
-          <Grid item className="center" xs={12} md={3}>
+        <Grid className="center" item xs={12} md={9}>
+          <Box>
             <Typography
-              sx={{ typography: { md: "h3", xs: "h4" } }}
+              sx={{ typography: { md: "h1", xs: "h2" } }}
+              style={{ paddingBottom: "2rem" }}
               color="common.white"
             >
-              準備中
+              raspi0124
             </Typography>
-          </Grid>
-          <Grid item className="center" xs={12} md={3}>
-            <Typography
-              sx={{ typography: { md: "h3", xs: "h4" } }}
-              color="common.white"
-            >
-              準備中
-            </Typography>
-          </Grid>
-          <Grid item className="center" xs={12} md={3}>
-            <Typography
-              sx={{ typography: { md: "h3", xs: "h4" } }}
-              color="common.white"
-            >
-              準備中
-            </Typography>
-          </Grid>
-          <Grid item className="center" xs={12} md={3}>
-            <Typography
-              sx={{ typography: { md: "h3", xs: "h4" } }}
-              color="common.white"
-            >
-              <Link href="/links">LINKS</Link>
-            </Typography>
-          </Grid>
+          </Box>
         </Grid>
-      </div>
-    </>
-  );
-};
+      </Grid>
+      <Grid className="menu" container spacing={3}>
+        <Grid item className="center" xs={12} md={3}>
+          <Typography
+            sx={{ typography: { md: "h3", xs: "h4" } }}
+            color="common.white"
+          >
+            準備中
+          </Typography>
+        </Grid>
+        <Grid item className="center" xs={12} md={3}>
+          <Typography
+            sx={{ typography: { md: "h3", xs: "h4" } }}
+            color="common.white"
+          >
+            準備中
+          </Typography>
+        </Grid>
+        <Grid item className="center" xs={12} md={3}>
+          <Typography
+            sx={{ typography: { md: "h3", xs: "h4" } }}
+            color="common.white"
+          >
+            準備中
+          </Typography>
+        </Grid>
+        <Grid item className="center" xs={12} md={3}>
+          <Typography
+            sx={{ typography: { md: "h3", xs: "h4" } }}
+            color="common.white"
+          >
+            <Link href="/links">LINKS</Link>
+          </Typography>
+        </Grid>
+      </Grid>
+    </div>
+  </>
+);
 
 export default TopDisplay;
